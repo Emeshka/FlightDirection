d801fdbab82b5a0760464910a6b096f88412835b
30028555 <alexa.emelina@gmail.com>
2022-03-13 23:33:12 +0300

    Destination, Article, Town controllers; Town get all in lang, post, destination put article, article get by destId and lang API; debugged all API; ResponseCountryPreview availableLanguages field;

---
+++ src/main/java/ru/emeshka/springreacttest/flightdirection/controller/ArticleController.java
@@ -0,0 +1,2 @@
+package ru.emeshka.springreacttest.flightdirection.controller;public class ArticleController {
+}
---
+++ src/main/java/ru/emeshka/springreacttest/flightdirection/message/ResponseArticle.java
@@ -0,0 +1,2 @@
+package ru.emeshka.springreacttest.flightdirection.message;public class ResponseArticle {
+}
